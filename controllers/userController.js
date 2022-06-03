bfc081b2fc62b4aabc0807391eee6ffa4ce13e6d
NjohPrince <jufredprince@gmail.com>
2022-06-03 01:44:49 +0100

    user fixes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,18 +1,25 @@
+const { ValidationError } = require("sequelize");
 const db = require("../models");
 
 const User = db.users;
 
 const createUser = async (req, res) => {
   let data = {
-    first_name: req.body.name,
-    last_name: req.body.name,
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
     contact: req.body.contact,
     email: req.body.email,
     password: req.body.password,
   };
 
-  const user = await User.create(data);
-  res.status(201).send(user);
+  try {
+    const user = await User.create(data);
+    res.status(201).send(user);
+  } catch (e) {
+    if (e instanceof ValidationError) {
+      return res.status(400).json({ error: e.errors[0].message });
+    }
+  }
 };
 
 const getUsers = async (req, res) => {
@@ -30,12 +37,13 @@ const updateUser = async (req, res) => {
   let id = req.params.id;
   const user = await User.update(req.body, { where: { id: id } });
   res.status(200).send(user);
+  console.log(user);
 };
 
 const deleteUser = async (req, res) => {
   let id = req.params.id;
   await User.destroy({ where: { id: id } });
-  res.status(200).send("User is deleted!");
+  res.status(200).json({ message: "User successfully deleted!" });
 };
 
 module.exports = {
